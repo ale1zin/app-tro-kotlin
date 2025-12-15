955e3b71473492768b7afe3fae5d8b67c3d763dc
Alê <45977655+ale1zin@users.noreply.github.com>
2025-12-15 20:54:09 -0300

    fix: gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.apptro"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
